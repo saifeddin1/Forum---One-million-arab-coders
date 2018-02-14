165b62c41298c2c58666c4cd92324fc457558d17
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-14 19:30:04 +0100

    Add files via upload

--- Forums/main.py
+++ Forums/main.py
@@ -1,16 +1,22 @@
-import models
+
+from models import Member, Post, MemberStore, Post_store
 
 
-member1 = models.Member("Mohamed", 15)
-member2 = models.Member("Karim", 25)
+member1 = Member("Mohamed", 15)
+member2 = Member("Karim", 25)
 
-post1 = models.Post("*Python", "Python Programming\n" *5)
-post2 = models.Post("*OOP", "Object Oriented Programming\n" *5)
-post3 = models.Post("*Nothing", "Nothing Here!\n" * 5)
+mymembers = MemberStore()
+mymembers = MemberStore()
+
+my_posts = Post_store()
+
+post1 = Post("*Python", "Python Programming\n" *5)
+post2 = Post("*OOP", "Object Oriented Programming\n" *5)
+post3 = Post("*Nothing", "Nothing Here!\n" * 5)
 #Starting
-print("******" * 15)
+print("*****" * 15)
 print("                       One Million Arab Coders Forum (sample)")
-print("******" * 15)
+print("*****" * 15)
 print("-" * 20, "\n", "Members")
 print("-" * 20)
 print(member1.name + "," + str(member1.age))
@@ -19,6 +25,17 @@ print(member2.name + "," + str(member2.age))
 print("-" * 20, "\n", "Posts")
 print("-" * 20)
 
-print(post1.title, "\n",post1.content)
-print(post2.title, "\n", post2.content)
-print(post3.title, "\n", post3.content)
+print(post1.title, "\n",post1.topic)
+print(post2.title, "\n", post2.topic)
+print(post3.title, "\n", post3.topic)
+
+mymembers.add(member1)
+mymembers.add(member2)
+mymembers.getall()
+print(mymembers)
+
+my_posts.add(post1)
+my_posts.add(post2)
+my_posts.add(post3)
+print(my_posts.get_all)
+
--- Forums/models.py
+++ Forums/models.py
@@ -2,8 +2,32 @@ class Member():
 	def __init__(self, name, age):
 		self.name = name
 		self.age = age
+
 class Post():
-	def __init__(self, title, content):
+	def __init__(self, title, topic):
 		self.title = title
-		self.content = content 
+		self.topic = topic 
+
+#~~~~~~~~~~~~~Stores~~~~~~~~~~~#
+class MemberStore():
+	members = []
 	
+	def add(self, member):
+		MemberStore.members.append(member)
+	
+	def getall(self):
+		for member in MemberStore.members:
+			return member
+		
+
+	
+class Post_store():
+	posts = []
+	
+	def add(self, post):
+		Post_store.posts.append(post)
+		
+	def get_all():
+		for post in Post_store.posts:
+			return post
+		
