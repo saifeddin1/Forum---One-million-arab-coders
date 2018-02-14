27006689982f0fa903b99cd7e25804489d4579c3
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-14 21:47:34 +0100

    Moved stores from here

--- Forums/models.py
+++ Forums/models.py
@@ -8,26 +8,4 @@ class Post():
 		self.title = title
 		self.topic = topic 
 
-#~~~~~~~~~~~~~Stores~~~~~~~~~~~#
-class MemberStore():
-	members = []
-	
-	def add(self, member):
-		MemberStore.members.append(member)
-	
-	def getall(self):
-		for member in MemberStore.members:
-			return self.members
-		
 
-	
-class Post_store():
-	posts = []
-	
-	def add(self, post):
-		Post_store.posts.append(post)
-		
-	def get_all():
-		for post in Post_store.posts:
-			return self.posts
-		
