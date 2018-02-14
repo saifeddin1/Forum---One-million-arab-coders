66d0f897e04c20e86515313fcab658f85bed3edf
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-14 21:48:14 +0100

    Moved stores here!

---
+++ Forums/stores.py
@@ -0,0 +1,23 @@
+#~~~~~~~~~~~~~Stores~~~~~~~~~~~#
+class MemberStore():
+	members = []
+	
+	def add(self, member):
+		MemberStore.members.append(member)
+	
+	def getall(self):
+		for member in MemberStore.members:
+			return self.members
+		
+
+	
+class Post_store():
+	posts = []
+	
+	def add(self, post):
+		Post_store.posts.append(post)
+		
+	def get_all():
+		for post in Post_store.posts:
+			return self.posts
+		
