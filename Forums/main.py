4425aae6ae1adf7783ec957196a1e9b2605cea5a
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-16 21:40:18 +0100

    Update main.py

--- Forums/main.py
+++ Forums/main.py
@@ -1,42 +1,47 @@
 
-from models import Member, Post
+#---------------------------------- Main -----------------------------------
+
+#Starting..
+
+      #Importing classes from the existing files models and stores
+import models
 from stores import MemberStore, PostStore
 
+#Creating members
+member1 = models.Member("Mohamed", 15)
+member2 = models.Member("Karim", 25)
 
-member1 = Member("Mohamed", 15)
-member2 = Member("Karim", 25)
-
-mymembers = MemberStore()
-mymembers = MemberStore()
-
+member_store = MemberStore()
 my_posts = PostStore()
 
-post1 = Post("*Python", "Python Programming\n" *5)
-post2 = Post("*OOP", "Object Oriented Programming\n" *5)
-post3 = Post("*Nothing", "Nothing Here!\n" * 5)
+#Decoration
+post1 = models.Post("*Python", "Python Programming\n" *5)
+post2 = models.Post("*OOP", "Object Oriented Programming\n" *5)
+post3 = models.Post("*Nothing", "Nothing Here!\n" * 5)
 #Starting
 print("*****" * 15)
 print("                       One Million Arab Coders Forum (sample)")
 print("*****" * 15)
 print("-" * 20, "\n", "Members")
 print("-" * 20)
+#Show member and their informations
 print(member1.name + "," + str(member1.age))
 print(member2.name + "," + str(member2.age))
-
 print("-" * 20, "\n", "Posts")
 print("-" * 20)
 
+#Posts Area
 print(post1.title, "\n",post1.topic)
 print(post2.title, "\n", post2.topic)
 print(post3.title, "\n", post3.topic)
+models.Post.separator()
 
-mymembers.add(member1)
-mymembers.add(member2)
-mymembers.get_all()
-print(mymembers)
-
+member_store.add(member1)
+member_store.add(member2)
+print(member_store.get_all())
 my_posts.add(post1)
 my_posts.add(post2)
 my_posts.add(post3)
+print(my_posts.get_all())
 print(my_posts.get_all)
 
