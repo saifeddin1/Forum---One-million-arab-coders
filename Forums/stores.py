97022e80620c1cbb8b5155543c6981e441019b88
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-19 16:30:37 +0100

    Update stores.py

--- Forums/stores.py
+++ Forums/stores.py
@@ -1,23 +1,45 @@
+
 #~~~~~~~~~~~~~Stores~~~~~~~~~~~#
 class MemberStore():
-	members = []
-	
-	def add(self, member):
-		MemberStore.members.append(member)
-	
-	def get_all(self):
-		for member in MemberStore.members:
-			return self.members
-		
+   
+    members = []
+    last_id = 1
+    
+    def add(self, member):
+        member.id = MemberStore.last_id
 
-	
+        MemberStore.members.append(member)
+        
+        MemberStore.last_id += 1
+    
+    def get_by_id(self, id):
+        all_members = self.get_all()
+        for member in all_members:
+            result = member
+        return result
+
+
+    def get_all(self):
+        for member in MemberStore.members:
+            return self.members
+    
+    def entity_exists(self, member):
+        result = True
+        if member.id in MemberStore:
+            return result
+        else:
+            return False
+        
+        
+        
+
+    
 class PostStore():
-	posts = []
-	
-	def add(self, post):
-		Post_store.posts.append(post)
-		
-	def get_all():
-		for post in Post_store.posts:
-			return self.posts
-		
+    posts = []
+    
+    def add(self, post):
+        PostStore.posts.append(post)
+        
+    def get_all(self):
+        for post in PostStore.posts:
+            return self.posts
