a5c7584a590a3bc20f7dd51adfcaf4d9a90d5b05
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-19 16:28:11 +0100

    Update models.py

--- Forums/models.py
+++ Forums/models.py
@@ -1,15 +1,18 @@
+
+#--------------------------- Models -------------------------------
+
 class Member():
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+    id = 0
+    def __init__(self, name, age, id):
+        self.id = 0
+        self.name = name
+        self.age = age
 
 class Post():
-	def __init__(self, title, topic):
-		self.title = title
-		self.topic = topic 
-	
-	@staticmethod
-        def separator():		
-            	print("=" * 40)
+    def __init__(self, title, topic):
+        self.title = title
+        self.topic = topic
 
-
+    @staticmethod
+    def separator():
+        print("=" * 40)
