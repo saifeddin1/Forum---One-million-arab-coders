93a6b318d7c44797a601ad86d433cf00681ae6ed
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-20 16:24:58 +0100

    Update stores.py

--- Forums/stores.py
+++ Forums/stores.py
@@ -1,45 +1,87 @@
 
 #~~~~~~~~~~~~~Stores~~~~~~~~~~~#
-class MemberStore():
-   
+class MemberStore:
     members = []
     last_id = 1
-    
+
     def add(self, member):
         member.id = MemberStore.last_id
 
         MemberStore.members.append(member)
-        
+
         MemberStore.last_id += 1
-    
+
+    def get_all(self):
+        return MemberStore.members
+
     def get_by_id(self, id):
+        result = None
         all_members = self.get_all()
+
         for member in all_members:
-            result = member
+            if id == member.id:
+                result = member
+                break
+
+        return result
+
+    def entity_exists(self, member):
+        result = True
+
+        if self.get_by_id(member.id) is None:
+            result = False
+        return result
+
+    def delete(self, id):
+        member = self.get_by_id(id)
+        MemberStore.members.remove(member)
+
+    def update(self, member):
+        all_members = self.get_all()
+        MemberStore.members = [member if member.id == current_member.id else current_member for current_member in all_members]
+
+    def get_by_name(self, name):
+        all_members = self.get_all()
+        result = []
+
+        for member in all_members:
+            if member.name == name:
+                result.append(member)
+
         return result
 
 
-    def get_all(self):
-        for member in MemberStore.members:
-            return self.members
-    
-    def entity_exists(self, member):
-        result = True
-        if member.id in MemberStore:
-            return result
-        else:
-            return False
-        
-        
-        
-
-    
-class PostStore():
+class PostStore:
     posts = []
-    
+    last_id = 1
+
     def add(self, post):
+        post.id = PostStore.last_id
+
         PostStore.posts.append(post)
-        
+
+        PostStore.last_id += 1
+
     def get_all(self):
+        return PostStore.posts
+
+    def get_by_id(self, id):
         for post in PostStore.posts:
-            return self.posts
+            if id == post.id:
+                return post
+        return None
+
+    def entity_exists(self, post):
+        result = True
+
+        if self.get_by_id(post.id) is None:
+            result = False
+        return result
+
+    def delete(self, id):
+        post = self.get_by_id(id)
+        PostStore.posts.remove(post)
+
+    def update(self, post):
+        all_posts = self.get_all()
+        PostStore.posts = [post if post.id == current_post.id else current_post for current_post in all_posts]
