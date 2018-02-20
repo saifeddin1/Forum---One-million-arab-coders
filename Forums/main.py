7b719093fee9e8105456dc0fae0314edce6c9ad0
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-20 16:33:09 +0100

    Update main.py

--- Forums/main.py
+++ Forums/main.py
@@ -4,45 +4,96 @@
 #Starting..
 
       #Importing classes from the existing files models and stores
+
 import models
-from stores import MemberStore, PostStore
+import stores
 
-#Creating members
-member1 = models.Member("Mohamed", 15, 1)
-member2 = models.Member("Karim", 25, 2)
 
-member_store = MemberStore()
-my_posts = PostStore()
+def create_members():
 
-#Decoration
-post1 = models.Post("*Python", "Python Programming\n" *5)
-post2 = models.Post("*OOP", "Object Oriented Programming\n" *5)
-post3 = models.Post("*Nothing", "Nothing Here!\n" * 5)
-#Starting
-print("*****" * 15)
-print("                       One Million Arab Coders Forum (sample)")
-print("*****" * 15)
-print("-" * 20, "\n", "Members")
-print("-" * 20)
-#Show member and their informations
-print(member1.name + "," + str(member1.age))
-print(member2.name + "," + str(member2.age))
-print("-" * 20, "\n", "Posts")
-print("-" * 20)
+    member1 = models.Member("Mohammed", 20)
+    member2 = models.Member("Omar", 22)
+    member3 = models.Member("Abdo", 25)
+    print(member1)
+    print(member2)
+    print(member3)
+    print("=" * 30)
 
-#Posts Area
-print(post1.title, "\n",post1.topic)
-print(post2.title, "\n", post2.topic)
-print(post3.title, "\n", post3.topic)
-models.Post.separator()
+    return member1, member2, member3
 
-member_store.add(member1)
-print(member_store.get_by_id(1))
-member_store.add(member2)
-print(member_store.get_by_id(2))
 
-print(member_store.get_all())
-my_posts.add(post1)
-my_posts.add(post2)
-my_posts.add(post3)
-print(my_posts.get_all())
+def store_should_add_models(members_instances, member_store):
+
+    for member in members_instances:
+        member_store.add(member)
+
+
+def stores_should_be_similar():
+
+    member_store1 = stores.MemberStore()
+    member_store2 = stores.MemberStore()
+    if member_store1.get_all() is member_store2.get_all():
+        print("Same stores elements !")
+
+
+def print_all_members(member_store):
+    print("=" * 30)
+
+    for member in member_store.get_all():
+        print(member)
+
+    print("=" * 30)
+
+
+def get_by_id_should_retrieve_same_object(member_store, member2):
+    member2_retrieved = member_store.get_by_id(member2.id)
+
+    if member2 is member2_retrieved:
+        print("member2 and member2_retrieved are matching !")
+
+
+def update_should_modify_object(member_store, member3):
+    member3_copy = models.Member(member3.name, member3.age)
+    member3_copy.id = 3
+
+    if member3_copy is not member3:
+        print("member3 and member3_copy are not the same !")
+
+    print(member3_copy)
+    member3_copy.name = "john"
+    member_store.update(member3_copy)
+    print(member_store.get_by_id(member3.id))
+
+
+def catch_exception_when_deleting():
+    try:
+        member_store.delete(5)
+    except ValueError:
+        print("It should be an existence entity before deleting !")
+
+
+def get_by_name_should_retrieve_all_members_with_name(member_store, name):
+    members_retrieved = member_store.get_by_name(name)
+    print(members_retrieved)
+
+
+members_instances = create_members()
+member1, member2, member3 = members_instances
+
+member_store = stores.MemberStore()
+
+
+get_by_name_should_retrieve_all_members_with_name(member_store, "Abdo")
+store_should_add_models(members_instances, member_store)
+
+stores_should_be_similar()
+
+print_all_members(member_store)
+
+get_by_id_should_retrieve_same_object(member_store, member2)
+
+update_should_modify_object(member_store, member3)
+
+catch_exception_when_deleting()
+
+print_all_members(member_store)
