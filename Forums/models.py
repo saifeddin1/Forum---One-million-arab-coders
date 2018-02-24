8f1ea2d3d61ee0a48ccf623d8de9fddcc9d525c3
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-24 16:39:47 +0100

    Update models.py

--- Forums/models.py
+++ Forums/models.py
@@ -1,18 +1,27 @@
 
 #--------------------------- Models -------------------------------
 
-class Member():
-    
-    def __init__(self, name, age, id):
+import datetime
+
+
+class Member:
+    def __init__(self, name, age):
         self.id = 0
         self.name = name
         self.age = age
+        self.posts = []
 
-class Post():
-    def __init__(self, title, content):
+    def __str__(self):
+        return "Name: {}, Age: {}".format(self.name, self.age)
+
+
+class Post:
+    def __init__(self, title, content, member_id=0):
+        self.id = 0
         self.title = title
         self.content = content
+        self.member_id = member_id
+        self.date = datetime.datetime.now()
 
-    @staticmethod
-    def separator():
-        print("=" * 40)
+    def __str__(self):
+return f"Title: {self.title}, Content: {self.content}, Date: {self.date}"
