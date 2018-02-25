74a45918994fc50761de9d9f14b7c1ea765407f5
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-25 17:48:30 +0100

    Update flask_app_demo.py

--- Flask_demo/flask_app_demo.py
+++ Flask_demo/flask_app_demo.py
@@ -2,14 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route("/index")
-
 def home():
 	return "Hi There"
 	
 @app.route("/SayHello/<name>")
-
 def say_hello(name):
-	return "Hello" + name + "!"
+	return "Hello %s " %s name
 
 
 app.run()
