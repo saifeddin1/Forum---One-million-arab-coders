be6f7ca8b1efee29a9de36f9e46cda14c3b1137f
Saifeddin Matoui <34710727+saifeddin1@users.noreply.github.com>
2018-02-25 17:10:13 +0100

    Add files via upload

--- Flask_demo/flask_app_demo.py
+++ Flask_demo/flask_app_demo.py
@@ -1,1 +1,15 @@
+from flask import Flask
 
+app = Flask(__name__)
+@app.route("/index")
+
+def home():
+	return "Hi There"
+	
+@app.route("/SayHello/<name>")
+
+def say_hello(name):
+	return "Hello" + name + "!"
+
+
+app.run()
